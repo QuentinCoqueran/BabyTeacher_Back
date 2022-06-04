ab4f1bd09fe81da1b1e01f7860e76e8b7a407279
David <zdavid1@myges.fr>
2022-06-04 14:53:59 +0200

    server listening

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import {config} from "dotenv";
+
+config();
+
+import express from "express";
+
+
+
+async function startServer(): Promise<void> {
+    const app = express();
+    app.listen(process.env.PORT, function () {
+        console.log("Server listening on port " + process.env.PORT);
+    });
+}
+
+startServer().catch(console.error);
