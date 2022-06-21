21179d24e2f658465f9ef85eb27cabe6b4e9cac3
David <zdavid1@myges.fr>
2022-06-21 16:04:37 +0200

    get skiill by user

--- controllers/categorie.controller.ts
+++ controllers/categorie.controller.ts
@@ -2,6 +2,7 @@ import express, {Request, Response, Router} from "express";
 import {AuthService} from "../services/auth.service";
 import {checkUserConnected} from "../middlewares/auth.middleware";
 import {CategorieService} from "../services/categorie.service";
+import {SkillService} from "../services/skill.service";
 
 
 export class CategorieController {
@@ -18,10 +19,25 @@ export class CategorieController {
         }
     }
 
+    async getSkillByUser(req: Request, res: Response) {
+        try {
+            const skills = await SkillService.getInstance().getByUser(req.params.login);
+            console.log(skills)
+            res.send({
+                response: skills
+            });
+        } catch (err) {
+            console.log(err)
+            res.status(401).end(); // unauthorized
+        }
+    }
+
 
     buildRoutes(): Router {
         const router = express.Router();
-        router.get('/getAllCategories',this.getAll.bind(this));
+        router.get('/getAllCategories', this.getAll.bind(this));
+        router.get('/getSkillsByUserId/:login', this.getSkillByUser.bind(this));
         return router;
     }
 }
+
