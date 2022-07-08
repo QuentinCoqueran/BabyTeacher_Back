1c97892b49770987cf11ebcaadd6f590974c48aa
Mathieu <ferreira.mathieu.pro@gmail.com>
2022-07-08 22:29:02 +0200

    add condition to update availability

--- services/availability.service.ts
+++ services/availability.service.ts
@@ -154,54 +154,37 @@ export class AvailabilityService {
         }))
     }
 
-    async updateListAvailabilityBabysitter(param: { arrayAvaibality: any, id: number }) {
-        let arrayAvaibality = param.arrayAvaibality;
-        for (let i = 0; i < arrayAvaibality.length; i++) {
-            let sql = `UPDATE availability SET day = '${arrayAvaibality[i].day}', startHour = ${arrayAvaibality[i].startHour}, endHour = ${arrayAvaibality[i].endHour} WHERE id = ${arrayAvaibality[i].id}`;
-            try {
+    async updateListAvailabilityBabysitter(param: { arrayAvaibality: any, idUser: number }) {
+        let arrayAvaibality = param.arrayAvaibality; //Ce que je reçois
+
+        let sqlQueries = [];
+        let userAvailability = await this.getByUserId(param.idUser);
+        let exist = false;
+        for (let avToAdd of arrayAvaibality){
+
+            for (let availability of userAvailability) {
+                if (avToAdd.day === availability.day) {
+                    exist = true;
+                    break;
+                }
+            }
+            if (!exist) {
+                sqlQueries.push(`UPDATE availability SET startHour = '${avToAdd.startHour}', endHour = '${avToAdd.endHour}' WHERE idUser = ${param.idUser} AND day = '${avToAdd.day}'`);
+            }else{
+                break;
+            }
+        }
+
+        if (!exist){
+            for (let sql of sqlQueries) {
                 await this.insertPromise(sql);
-            } catch (error) {
-                console.log(error);
-                throw new Error("Error in update session");
             }
+            return Promise.resolve("Availability updated");
+        }else {
+            return Promise.reject("One of the availability already exist (Rollback)");
         }
     }
 
-    public async update(availability: AvailabilityProps) {
-        if (availability.idUser && availability.idPost) {
-            let idUser = availability.idUser;
-            let role;
-
-            role = await AuthService.getInstance().getRoleByUserId(idUser.toString());
-
-            if (role[0].role === "parent") {
-                return Promise.reject("Parent can't add availability on their profile");
-            }
-
-            let idPost = availability.idPost;
-            let exist = await this.getByPostId(idPost);
-
-            let cpt = 0;
-            while (exist[cpt]) {
-                if (exist[cpt].id === availability.id) {
-                    cpt++;
-                } else {
-                    return Promise.reject("Availability already exist for this post");
-                }
-            }
-        }
-
-
-        let sqlQuery = `UPDATE availability SET idUser = ${availability.idUser}, idPost = ${availability.idPost}, day = '${availability.day}', startHour = '${availability.startHour}', endHour = '${availability.endHour}' WHERE id = ${availability.id}`
-        return new Promise<RowDataPacket[]>(((resolve, reject) => {
-            db.query(sqlQuery, (error: QueryError, results: RowDataPacket[]) => {
-                if (error) {
-                    return reject(error)
-                }
-                return resolve(results);
-            })
-        }))
-    }
 
     public async delete(id: number) {
         let sqlQuery = `DELETE FROM availability WHERE id = ${id}`
