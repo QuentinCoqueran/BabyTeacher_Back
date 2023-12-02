27a19c4d1be41fb815c4c5840d4a6435abcbf567
QuentinCoqueran <coqueranquentin@gmail.com>
2023-12-02 21:04:01 +0100

    dockerfile for mongo

--- init-mongo.js
+++ init-mongo.js
@@ -1,3 +1,7 @@
+db = connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_USERNAME}@localhost:27017/admin`);
+
+db = db.getSiblingDB(`babyteacher`);
+
 db.createUser(
     {
         user: process.env.MONGO_INITDB_ROOT_USERNAME,
